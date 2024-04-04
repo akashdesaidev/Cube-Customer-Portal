4676e222564f84b0000cb2d9ad4eb18cae66baef
akashdesaimasai <akash.desai3105@gail.com>
2024-04-04 19:07:15 +0530

    added 1000 data for costumer

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,53 +1,18 @@
 export interface Customer {
-    id: number;
-    name: string;
-    title: string;
-  }
+  id: number;
+  name: string;
+  title: string;
+}
 
-export const customers:Customer[]=[
-    {
-      id:1,
-      name:"Costumer1",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-    {
-      id:2,
-      name:"Costumer2",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-    {
-      id:3,
-      name:"Costumer3",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-    {
-      id:4,
-      name:"Costumer4",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-    {
-      id:5,
-      name:"Costumer5",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-    {
-      id:6,
-      name:"Costumer6",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-    {
-      id:7,
-      name:"Costumer7",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-    {
-      id:8,
-      name:"Costumer8",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-    {
-      id:9,
-      name:"Costumer9",
-      title:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam."
-    },
-  ]
+export const customers: Customer[] = [];
+
+// Generate 1000 customer data entries
+for (let i = 1; i <= 1000; i++) {
+  customers.push({
+    id: i,
+    name: `Customer${i}`,
+    title:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum doloribus dolore quas commodi? Quaerat debitis in possimus assumenda harum ut ex laudantium, quam provident similique libero exercitationem nisi ea. Natus Incidunt, natus esse, unde consequatur alias, cumque optio exercitationem voluptatem blanditiis et eos officia voluptate totam accusantium pariatur! Asperiores eaque, vero dolorem repellat itaque nam quibusdam molestias iure nemo ullam.",
+  });
+}
+console.log(customers);
